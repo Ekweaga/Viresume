afb8928365e6442018a198672b95720a15250b2a
Ekweaga <hikay133@gmail.com>
2023-01-01 02:18:29 +0100

    nav design

---
+++ components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Image from "next/image"
+
+function Navbar() {
+  return (
+   <>
+   <nav className='text-[#4E57A9] flex justify-between p-4'>
+    <div>
+        <Image src="/Group 8.png" width={100} height={150} alt="icon"/>
+
+    </div>
+    <div>
+        <ul className='flex gap-[20px]'>
+            <li>About</li>
+            <li>Pricing</li>
+            <li>Testimonials</li>
+            <li>Blog</li>
+        </ul>
+    </div>
+    <div className='flex gap-[20px] align-items-center justify-items-center'>
+        <button className='bg-transparent p-2 rounded-md w-[100px]'>Sign in</button>
+        <button className='bg-[#010D82] text-white p-2 rounded-md w-[100px]'>Sign up</button>
+    </div>
+   </nav>
+   </>
+  )
+}
+
+export default Navbar
